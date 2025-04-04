2fd6faa08cfcf62e604f695be2838d0622776d80
Guh-paixao <gustavopaixao.dev@gmail.com>
2025-04-04 10:18:53 -0300

    add OpenAPI documentation support and remove unused security dependencies

--- src/main/java/br/com/gustavo/inventarioAPI/InventarioApplication.java
+++ src/main/java/br/com/gustavo/inventarioAPI/InventarioApplication.java
@@ -1,9 +1,18 @@
 package br.com.gustavo.inventarioAPI;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Inventário API",
+                version = "1.0",
+                description = "API para gerenciamento de inventário"
+        )
+)
 public class InventarioApplication {
 
     public static void main(String[] args) {
